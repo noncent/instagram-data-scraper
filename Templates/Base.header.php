671d4f4f1ee4cc255a57c986e719ac7454bfbeab
Neeraj Singh <neerajsinghsonu@gmail.com>
2017-06-17 20:07:11 +0530

    Chnage page title

--- Templates/Base.header.php
+++ Templates/Base.header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="expires" content="0" />
     <meta name="robots" content="noindex, follow" />
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-    <title>D-Scrapper | Get Instagram Data</title>
+    <title>Instagram Super Scraper | Get Instagram Data</title>
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="<?=assets('assets/css/app.css')?>" /> 
